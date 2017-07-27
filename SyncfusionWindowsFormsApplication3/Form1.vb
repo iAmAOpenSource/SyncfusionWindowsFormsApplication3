8dc3c8168a9f5a06cf4bbc80ddeb97c6423bfa1d
iAmAOpenSource <lamborguini00@hotmail.com>
2017-07-27 18:59:04 +0100

    Test

--- SyncfusionWindowsFormsApplication3/Form1.vb
+++ SyncfusionWindowsFormsApplication3/Form1.vb
@@ -11,38 +11,39 @@ Public Class Form1
         ' store error output lines
 
         Dim executable As String() = {"adb", "adb"}
-        Dim arguments As String() = {"help", "reboot"}
-        For i As Integer = 0 To 0
-            Dim process = New Process()
-            process.StartInfo = createStartInfo(executable(i), arguments(i))
-            process.EnableRaisingEvents = True
-            AddHandler process.Exited, Sub(ByVal sendera As Object, ByVal ea As System.EventArgs)
-                                           Console.WriteLine(process.ExitTime)
-                                           Console.WriteLine(". Processing done.")
-                                           'UpdateTextBox(ea)
+        Dim arguments As String() = {"adb help", " adb reboot"}
+        For i As Integer = 0 To arguments.Length - 1
+            MsgBox(adb(arguments(i)))
+            'Dim process = New Process()
+            'process.StartInfo = createStartInfo(executable(i), arguments(i))
+            'process.EnableRaisingEvents = True
+            'AddHandler process.Exited, Sub(ByVal sendera As Object, ByVal ea As System.EventArgs)
+            '                               Console.WriteLine(process.ExitTime)
+            '                               Console.WriteLine(". Processing done.")
+            '                               'UpdateTextBox(ea)
 
-                                       End Sub
-            ' catch standard output
-            AddHandler process.OutputDataReceived, Sub(ByVal senderb As Object, ByVal eb As DataReceivedEventArgs)
-                                                       If (Not String.IsNullOrEmpty(eb.Data)) Then
-                                                           Console.WriteLine(String.Format("{0}> {1}", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"), eb.Data))
-                                                           'UpdateTextBox(eb.Data)
+            '                           End Sub
+            '' catch standard output
+            'AddHandler process.OutputDataReceived, Sub(ByVal senderb As Object, ByVal eb As DataReceivedEventArgs)
+            '                                           If (Not String.IsNullOrEmpty(eb.Data)) Then
+            '                                               Console.WriteLine(String.Format("{0}> {1}", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"), eb.Data))
+            '                                               'UpdateTextBox(eb.Data)
 
-                                                       End If
-                                                   End Sub
-            ' catch errors
-            AddHandler process.ErrorDataReceived, Sub(ByVal senderc As Object, ByVal ec As DataReceivedEventArgs)
-                                                      Console.WriteLine(String.Format("! {0}", ec.Data))
-                                                      Dim a As String = String.Format("! {0}", ec.Data)
-                                                      'UpdateTextBox(a)
-                                                  End Sub
-            ' start process
-            Dim result = process.Start()
-            ' and wait for output
-            process.BeginOutputReadLine()
-            ' and wait for errors :-)
-            process.BeginErrorReadLine()
-            process.WaitForExit()
+            '                                           End If
+            '                                       End Sub
+            '' catch errors
+            'AddHandler process.ErrorDataReceived, Sub(ByVal senderc As Object, ByVal ec As DataReceivedEventArgs)
+            '                                          Console.WriteLine(String.Format("! {0}", ec.Data))
+            '                                          Dim a As String = String.Format("! {0}", ec.Data)
+            '                                          'UpdateTextBox(a)
+            '                                      End Sub
+            '' start process
+            'Dim result = process.Start()
+            '' and wait for output
+            'process.BeginOutputReadLine()
+            '' and wait for errors :-)
+            'process.BeginErrorReadLine()
+            'process.WaitForExit()
 
         Next
 
@@ -54,7 +55,7 @@ Public Class Form1
             Me.Invoke(New Action(Of String)(AddressOf UpdateTextBox), args)
             Return
         End If
-        Label1.Text += a
+        Label1.Text += "a"
     End Sub
 
     Private Function createStartInfo(ByVal executable As String, ByVal arguments As String) As ProcessStartInfo
@@ -70,5 +71,39 @@ Public Class Form1
         Return processStartInfo
 
     End Function
+
+    Function adb(ByVal Arguments As String) As String
+        Try
+
+            Dim My_Process As New Process()
+            Dim My_Process_Info As New ProcessStartInfo()
+
+            My_Process_Info.FileName = "cmd.exe" ' Process filename
+            My_Process_Info.Arguments = Arguments ' Process arguments
+            'My_Process_Info.WorkingDirectory = "C:\Users\<Your User Name>\AppData\Local\Android\android-sdk\platform-tools" 'this directory can be different in your case.
+            My_Process_Info.CreateNoWindow = True  ' Show or hide the process Window
+            My_Process_Info.UseShellExecute = False ' Don't use system shell to execute the process
+            My_Process_Info.RedirectStandardOutput = True  '  Redirect (1) Output
+            My_Process_Info.RedirectStandardError = True  ' Redirect non (1) Output
+
+            My_Process.EnableRaisingEvents = True ' Raise events
+            My_Process.StartInfo = My_Process_Info
+            My_Process.Start() ' Run the process NOW
+
+            Dim Process_ErrorOutput As String = My_Process.StandardOutput.ReadToEnd() ' Stores the Error Output (If any)
+            Dim Process_StandardOutput As String = My_Process.StandardOutput.ReadToEnd() ' Stores the Standard Output (If any)
+
+            ' Return output by priority
+            If Process_ErrorOutput IsNot Nothing Then Return Process_ErrorOutput ' Returns the ErrorOutput (if any)
+            If Process_StandardOutput IsNot Nothing Then Return Process_StandardOutput ' Returns the StandardOutput (if any)
+
+        Catch ex As Exception
+            Return ex.Message
+        End Try
+
+        Return "OK"
+
+    End Function
+
 
 End Class
