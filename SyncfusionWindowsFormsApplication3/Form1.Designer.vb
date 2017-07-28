ae40eb61035635af7752197f786f3ab8b47d73e6
iAmAOpenSource <lamborguini00@hotmail.com>
2017-07-28 14:15:47 +0100

    Fixed Problem

--- SyncfusionWindowsFormsApplication3/Form1.Designer.vb
+++ SyncfusionWindowsFormsApplication3/Form1.Designer.vb
@@ -23,7 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Button1
@@ -35,21 +35,23 @@ Partial Class Form1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Label1
+        'TextBox1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(293, 102)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "A label"
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Location = New System.Drawing.Point(161, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(433, 460)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.WordWrap = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(618, 486)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -59,5 +61,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Button1 As Button
-    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- SyncfusionWindowsFormsApplication3/Form1.vb
+++ SyncfusionWindowsFormsApplication3/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.IO
 Imports System.Diagnostics
+Imports System.Threading
 
 Public Class Form1
 
@@ -8,44 +9,8 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        ' store error output lines
 
-        Dim executable As String() = {"adb", "adb"}
-        Dim arguments As String() = {"adb help", " adb reboot"}
-        For i As Integer = 0 To arguments.Length - 1
-            MsgBox(adb(arguments(i)))
-            'Dim process = New Process()
-            'process.StartInfo = createStartInfo(executable(i), arguments(i))
-            'process.EnableRaisingEvents = True
-            'AddHandler process.Exited, Sub(ByVal sendera As Object, ByVal ea As System.EventArgs)
-            '                               Console.WriteLine(process.ExitTime)
-            '                               Console.WriteLine(". Processing done.")
-            '                               'UpdateTextBox(ea)
-
-            '                           End Sub
-            '' catch standard output
-            'AddHandler process.OutputDataReceived, Sub(ByVal senderb As Object, ByVal eb As DataReceivedEventArgs)
-            '                                           If (Not String.IsNullOrEmpty(eb.Data)) Then
-            '                                               Console.WriteLine(String.Format("{0}> {1}", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"), eb.Data))
-            '                                               'UpdateTextBox(eb.Data)
-
-            '                                           End If
-            '                                       End Sub
-            '' catch errors
-            'AddHandler process.ErrorDataReceived, Sub(ByVal senderc As Object, ByVal ec As DataReceivedEventArgs)
-            '                                          Console.WriteLine(String.Format("! {0}", ec.Data))
-            '                                          Dim a As String = String.Format("! {0}", ec.Data)
-            '                                          'UpdateTextBox(a)
-            '                                      End Sub
-            '' start process
-            'Dim result = process.Start()
-            '' and wait for output
-            'process.BeginOutputReadLine()
-            '' and wait for errors :-)
-            'process.BeginErrorReadLine()
-            'process.WaitForExit()
-
-        Next
+        Task.Run(Sub() runCommands())
 
     End Sub
 
@@ -55,7 +20,8 @@ Public Class Form1
             Me.Invoke(New Action(Of String)(AddressOf UpdateTextBox), args)
             Return
         End If
-        Label1.Text += "a"
+        TextBox1.Text += (a & Environment.NewLine)
+
     End Sub
 
     Private Function createStartInfo(ByVal executable As String, ByVal arguments As String) As ProcessStartInfo
@@ -69,41 +35,48 @@ Public Class Form1
         processStartInfo.ErrorDialog = False
         processStartInfo.CreateNoWindow = True
         Return processStartInfo
-        aaa
     End Function
 
-    Function adb(ByVal Arguments As String) As String
-        Try
+    Private Sub runCommands()
+        Dim executable As String() = {"adb", "adb"}
+        Dim arguments As String() = {"help", "reboot"}
+        For i As Integer = 0 To arguments.Length - 1
 
-            Dim My_Process As New Process()
-            Dim My_Process_Info As New ProcessStartInfo()
+            Dim process = New Process()
+            process.StartInfo = createStartInfo(executable(i), arguments(i))
+            process.EnableRaisingEvents = True
+            AddHandler process.Exited, Sub(ByVal sendera As Object, ByVal ea As System.EventArgs)
+                                           Console.WriteLine(process.ExitTime)
+                                           Console.WriteLine(". Processing done.")
+                                           UpdateTextBox(Environment.NewLine & process.ExitTime & Environment.NewLine & ".ProcessingDone")
 
-            My_Process_Info.FileName = "cmd.exe" ' Process filename
-            My_Process_Info.Arguments = Arguments ' Process arguments
-            'My_Process_Info.WorkingDirectory = "C:\Users\<Your User Name>\AppData\Local\Android\android-sdk\platform-tools" 'this directory can be different in your case.
-            My_Process_Info.CreateNoWindow = True  ' Show or hide the process Window
-            My_Process_Info.UseShellExecute = False ' Don't use system shell to execute the process
-            My_Process_Info.RedirectStandardOutput = True  '  Redirect (1) Output
-            My_Process_Info.RedirectStandardError = True  ' Redirect non (1) Output
+                                       End Sub
+            ' catch standard output
+            AddHandler process.OutputDataReceived, Sub(ByVal senderb As Object, ByVal eb As DataReceivedEventArgs)
+                                                       If (Not String.IsNullOrEmpty(eb.Data)) Then
+                                                           Dim b As String = String.Format("{0}> {1}", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"), eb.Data)
+                                                           Console.WriteLine(b)
+                                                           UpdateTextBox(b)
 
-            My_Process.EnableRaisingEvents = True ' Raise events
-            My_Process.StartInfo = My_Process_Info
-            My_Process.Start() ' Run the process NOW
+                                                       End If
+                                                   End Sub
+            ' catch errors
+            AddHandler process.ErrorDataReceived, Sub(ByVal senderc As Object, ByVal ec As DataReceivedEventArgs)
+                                                      Dim a As String = String.Format("! {0}", ec.Data)
+                                                      Console.WriteLine(a)
+                                                      UpdateTextBox(a)
+                                                  End Sub
+            ' start process
+            Dim result = process.Start()
+            ' and wait for output
+            process.BeginOutputReadLine()
+            ' and wait for errors :-)
+            process.BeginErrorReadLine()
+            'process.WaitForExit()
+            process.WaitForExit()
 
-            Dim Process_ErrorOutput As String = My_Process.StandardOutput.ReadToEnd() ' Stores the Error Output (If any)
-            Dim Process_StandardOutput As String = My_Process.StandardOutput.ReadToEnd() ' Stores the Standard Output (If any)
+        Next
 
-            ' Return output by priority
-            If Process_ErrorOutput IsNot Nothing Then Return Process_ErrorOutput ' Returns the ErrorOutput (if any)
-            If Process_StandardOutput IsNot Nothing Then Return Process_StandardOutput ' Returns the StandardOutput (if any)
-
-        Catch ex As Exception
-            Return ex.Message
-        End Try
-
-        Return "OK"
-
-    End Function
-
+    End Sub
 
 End Class
